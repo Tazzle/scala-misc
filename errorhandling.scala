bd4f94887fbc44e1c8f84c7af5a187e80cfaa17d
Tazzle <tazzle@tazzle.com>
2018-05-10 20:31:51 +0100

    exercise 4.1. flatMap and getOrElse

--- errorhandling.scala
+++ errorhandling.scala
@@ -14,3 +14,25 @@ case object None extends Option[Nothing]
 
 val integerOption = Some(1)
 integerOption.map(x => x.toDouble)
+
+// 2) flatMap 3) getOrElse
+trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(value) => Some(f(value))
+    case None        => None
+  }
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(value) => value
+    case None => default
+  }
+    def flatMap[B](f: A => Option[B]): Option[B] = {
+      this.map(x => f(x)).getOrElse(None)
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+val integerOption = Some(16)
+None.getOrElse(2)
+integerOption.flatMap(x => Some(x.toDouble))
