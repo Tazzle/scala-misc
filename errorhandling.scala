ed09a4b06989f43ad9ec727fa3e81fee3c10c284
Tazzle <tazzle@tazzle.com>
2018-05-11 21:29:01 +0100

    exercise 4.1.  method. Refactor current code to show only one Option Trait with all methods defined underneath.

--- errorhandling.scala
+++ errorhandling.scala
@@ -1,21 +1,11 @@
 //Chapter 4: Handling Errors without Exceptions
 
-//exercise 4.:1 Implement all functionso on Option
+//exercise 4.:1 Implement all functions on Option
 
-// 1) map
-trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case Some(value) => Some(f(value))
-    case None => None
-  }
-}
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
-
-val integerOption = Some(1)
-integerOption.map(x => x.toDouble)
-
-// 2) flatMap 3) getOrElse
+//exercise 4.1
+//implement functions on Option. You should be able to implement
+//all the functions besides `map` or `getOrElse` without resorting
+//to pattern matching.
 trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
     case Some(value) => Some(f(value))
@@ -23,38 +13,25 @@ trait Option[+A] {
   }
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(value) => value
-    case None => default
+    case None        => default
   }
-    def flatMap[B](f: A => Option[B]): Option[B] = {
-      this.map(x => f(x)).getOrElse(None)
-  }
-}
-
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
-
-val integerOption = Some(16)
-None.getOrElse(2)
-integerOption.flatMap(x => Some(x.toDouble))
-
-
-// 4) or else
-trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case Some(value) => Some(f(value))
-    case None        => None
-  }
-  def getOrElse[B >: A](default: => B): B = this match {
-    case Some(value) => value
-    case None => default
-  }
-    def flatMap[B](f: A => Option[B]): Option[B] = {
-      this.map(x => f(x)).getOrElse(None)
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    this.map(x => f(x)).getOrElse(None)
   }
   def orElse[B >: A](ob: => Option[B]): Option[B] = {
-    if(this == None) ob
+    if (this == None) ob
     else this
+  }
+  def filter(f: A => Boolean): Option[A] = {
+    this.flatMap(x => if (f(x)) Some(x) else None)
   }
 }
 
-integerOption.orElse(Some(2))
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+Some(16).map(x => x.toDouble)
+None.getOrElse(2)
+Some(1).flatMap(x => Some(x.toDouble))
+None.orElse(Some(2))
+Some(1).filter(x => x > 1)
